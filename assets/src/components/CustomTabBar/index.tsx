67271d165d552044eb4db6876194f253f0d238f9
HenriqueCerrano <henrique.cerrano@gmail.com>
2025-09-15 11:46:14 -0300

    Criando botoes para cada componente da segunda tela, depois aplicando icones para melhorar performace e centralizar eles

--- assets/src/components/CustomTabBar/index.tsx
+++ assets/src/components/CustomTabBar/index.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { Text, Touchable, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
+import { AntDesign, FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
 
 export default ({state, navigation}: any) => {
    return(
    <View style={style.tabArea}>
       <TouchableOpacity>
-         <Text>Esquerda</Text>
+         <AntDesign
+         name="bars"
+         style={{fontSize: 32}}
+         />
       </TouchableOpacity>
-      <TouchableOpacity>
-         <Text>Centro</Text>
+      <TouchableOpacity style={style.tabItemButtom}>
+         <View>
+            <Entypo 
+            name='plus'
+            size={40}
+            />
+         </View>
+         
       </TouchableOpacity>
-      <TouchableOpacity>
-         <Text>Direita</Text>
+      <TouchableOpacity style={style.tabItem}>
+         <FontAwesome
+         name='user'
+         style={{ fontSize: 32}}
+         />
+         
       </TouchableOpacity>
 
    </View>
